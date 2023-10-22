bd79dd2553149c32330838947d428e2d9aef3c4c
samanemkul <nemkulsama1@gmail.com>
2023-10-22 10:03:54 +0545

    Space shooter

---
+++ player.js
@@ -0,0 +1,33 @@
+class Player{
+    constructor(){
+        this.position ={
+            x:canvas.width/2.8,
+            y:canvas.height/2.7,
+        };
+        this.velocity ={
+            x:0,
+            y:0,
+        };
+        this.isAlive = true;
+        this.size = 50;
+        this.image = new Image();
+        this.image.src = "./image/fighter.png"
+    }
+    draw(){
+        c.beginPath();
+        c.fillStyle ="red";
+        c.drawImage(this.image,this.position.x,this.position.y, this.size,this.size);
+    }
+    move(){
+        this.position.x+=this.velocity.x;
+        this.position.y+=this.velocity.y;
+    }
+
+    update(){
+        this.draw();
+        if(this.isAlive){
+            this.move();
+        }
+        
+    }
+}
